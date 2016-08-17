4ffa017793eee721090a099fe4124402d74d0494
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-08-17 11:52:37 +0300

    Sinleton class added to provide singleton pattern behavior

---
+++ application/Singleton.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bohdan
+ * Date: 8/17/16
+ * Time: 10:02 AM
+ */
+
+namespace App;
+
+
+abstract class Singleton
+{
+    private static $instances = array();
+
+    public static function getInstance($className = false)
+    {
+        $keyClassName = ($className === false) ? get_called_class() : $className;
+        if(class_exists($keyClassName))
+        {
+            if(!isset($instances[$keyClassName]))
+                self::$instances[$keyClassName] = new $keyClassName();
+            return self::$instances[$keyClassName];
+        }
+
+        throw new \Exception('Class '.$keyClassName.' does not exist!');
+    }
+
+    final private function __clone(){}
+}
