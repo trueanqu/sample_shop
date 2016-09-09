b5a2e761274f3eced82bdf7a47869f93ddda4b8d
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-09-09 09:16:41 +0300

    add methods high level templates (should be checked)

--- vendor/trueanqu/Framework/DB/DBObject.php
+++ vendor/trueanqu/Framework/DB/DBObject.php
@@ -8,8 +8,48 @@
 
 namespace Framework\DB;
 
-
+/**
+ * Interface DBObject is unified interface which presents a set of required methods to work with various DBs
+ * For current DB architecture (metamodel) framework should work with one db(or scheme) per app
+ * For now, framework won't contain db/db scheme creation, this should developer do using db administration tools
+ * it will be considered that db, specified in configuration file was created and works correctly
+ *
+ * @package Framework\DB
+ */
 interface DBObject
 {
+    /**
+     * Method to get category tree from database to use as navigation menu
+     * @return mixed
+     */
+    function getCategoryTree();
+
+    /** Get certain client's orders
+     * @param $client client identifier
+     * @return mixed
+     */
+    function getClientOrders($client);
+
+    /**
+     * Get specific order ...
+     * @param $order order identifier
+     * @return mixed
+     */
+    function getOrder($order);
+
+    /**
+     * Get all orders
+     * @return mixed
+     */
+    function getOrders();
+
+
+    /**
+     * Get category with child elements of lower level ($recursively = FALSE) or with child elements of all lower levels ($recursively = TRUE)
+     * @param $category
+     * @param bool $recursively
+     * @return mixed
+     */
+    function getCategory($category, $recursively = FALSE);
 
 }
