6f4433fab1e7d17bda4bf5a6864da9f5d394bf34
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-09-22 13:42:28 +0300

    implement minimal request class

--- vendor/trueanqu/Framework/Request.php
+++ vendor/trueanqu/Framework/Request.php
@@ -11,5 +11,31 @@ namespace Framework;
 
 class Request
 {
+    private $requestUri;
+    private $requestMethod;
 
+    public function __construct()
+    {
+        $this->requestMethod = $this->filterMethod();
+        $this->requestUri = $this->filterUri();
+    }
+
+    public function filterUri()
+    {
+        return filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_ENCODED, FILTER_FLAG_ENCODE_HIGH);
+    }
+
+    /**
+     * @return bool
+     */
+    public function filterMethod()
+    {
+        $validMethods = array ('GET','POST', 'PUT', 'DELETE');
+        if(in_array($_SERVER['REQUEST_METHOD'],$validMethods))
+        {
+            return $_SERVER['REQUEST_METHOD'];
+        } else {
+            //TODO: return response with 500 code or throw exception;
+        }
+    }
 }
