891d61db105a8e88b5ad47e13591778cde71cbf4
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-09-29 10:20:01 +0300

    initial router. Implementation is needed

--- vendor/trueanqu/Framework/Router.php
+++ vendor/trueanqu/Framework/Router.php
@@ -9,7 +9,42 @@
 namespace Framework;
 
 
-class Router
+class Router extends Singleton
 {
+    private $routes = array();
 
+    private function __construct()
+    {
+        $this->getRoutesFromConfig();
+    }
+
+    protected static function createInstance()
+    {
+        return new Router();
+    }
+
+    private function getRoutesFromConfig()
+    {
+        $config = Config::getInstance();
+        $this->routes = $config->getConfigByName('routes');
+        $this->parsePatterns();
+    }
+
+    private function parsePatterns()
+    {
+        foreach ($this->routes as $routerKey => $config)
+        {
+            //@TODO: fix pattern replacing
+            str_replace('/','\/',$config['pattern']);
+            $reqKeys = preg_match('%\{[a-z]+\}%', $config['pattern']);
+            if(isset($config['requirements'][$reqKeys[1]]))
+                preg_replace('%\{([a-z]+?)\}%',$config['requirements'][$reqKeys[1]], $config['pattern']);
+            $config['pattern'] = '/^'.$config['pattern'].'$/';
+        }
+    }
+
+    public function getRoute(Request $request)
+    {
+
+    }
 }
