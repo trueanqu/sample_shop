d44c22fe6db03b71687143e4c899a5b48a40a8b1
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-10-03 08:47:59 +0300

    fix uri filtering rule

--- vendor/trueanqu/Framework/Request.php
+++ vendor/trueanqu/Framework/Request.php
@@ -38,7 +38,7 @@ class Request extends Singleton
 
     public function filterUri()
     {
-        if(preg_match("/^\/[a-z]+(\/([a-z]+|[0-9]+))?$/", $_SERVER['REQUEST_URI'])) // deleted (\/[a-z]+)? for '/edit'-like suffixes
+        if(preg_match("/^\/[a-z]*(\/([a-z]*|[0-9]*))?$/", $_SERVER['REQUEST_URI'])) // deleted (\/[a-z]+)? for '/edit'-like suffixes
             return $_SERVER['REQUEST_URI'];
         else
             return 'invalid uri';//TODO: handle exception
