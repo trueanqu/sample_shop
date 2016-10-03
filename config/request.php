91f2bdcbf4e868900a8c8d57580d608411c3c6f6
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-10-03 09:24:19 +0300

    add valid http methods

--- config/request.php
+++ config/request.php
@@ -1,4 +1,10 @@
 <?php
 return [
-    'uri_filter' => '/^\/[a-z]*(\/([a-z]*|[0-9]*))?$/'
+    'uri_filter' => '/^\/[a-z]*(\/([a-z]*|[0-9]*))?$/',
+    'valid_http_methods' => [
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE'
+    ]
 ];
