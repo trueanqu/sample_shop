64972f7156d6295ac7ca7816986e1c38606b8b9c
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-10-17 13:38:51 +0300

    initial factory for db objects

--- vendor/trueanqu/Framework/DB/DBObjectManager.php
+++ vendor/trueanqu/Framework/DB/DBObjectManager.php
@@ -3,7 +3,19 @@
 namespace Framework\DB;
 
 
+use Framework\DB\Adapters\MySQLAdapter;
+
 class DBObjectManager
 {
+    public static function getDBObject($driver = 'mysqli')
+    {
+        switch($driver) {
+            case 'mysqli':
+                return new MySQLAdapter();
+                break;
+            default :
+                throw new \Exception('No '.$driver.' database driver found.');
+        }
 
+    }
 }
