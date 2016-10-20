ff1bb8fda45ddab9e42a4c6244f14db7871b8645
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2016-10-20 11:34:33 +0300

    add username db config entry

--- config/default_config.php
+++ config/default_config.php
@@ -14,6 +14,7 @@ return [
     'db' => [
         'dbms' => 'mysql',
         'host' => '127.0.0.1',
+        'user' => 'root',
         'port' => '3306',
         'database' => 'sample_shop_db'
     ],
